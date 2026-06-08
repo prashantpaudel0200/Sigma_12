a33d86c3a2abd2fa2cfaea09450502c9abe38189
Prashant Paudel <prashantpdl0200@gmail.com>
2026-06-08 15:33:26 +0545

    Practice Question solved

---
+++ Javascript/product.js
@@ -0,0 +1,18 @@
+const product= {
+    name: "Black Pen",
+    rating:4,
+    price:200,
+    offer: 20,
+};
+console.log(product);
+
+const profile = {
+    userName : "Prashant Paudel",
+    post : 5,
+    followers : 280,
+    following: 250,
+    bio : "Free Sprit",
+};
+console.log(profile);
+console.log(typeof profile);
+console.log(typeof profile.userName);
