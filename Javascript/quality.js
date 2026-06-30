d00cde30494b1592d6eb83c13ec57e08fb9bf345
Prashant Paudel <prashantpdl0200@gmail.com>
2026-06-30 22:41:17 +0545

    Boolean operators

---
+++ Javascript/quality.js
@@ -0,0 +1,7 @@
+let string=prompt("Enter sample string");
+let length=string.length;
+if(string[0]==='a' && length>3){
+    console.log("This is a good string");
+} else{
+    console.log("This is not a good string");
+}
