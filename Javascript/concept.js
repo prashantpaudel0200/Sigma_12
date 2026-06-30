aeb4bc9352abc835d29d720aeab8306b5caa94e0
Prashant Paudel <prashantpdl0200@gmail.com>
2026-06-30 19:14:04 +0545

    Practise Qs completed

--- Javascript/concept.js
+++ Javascript/concept.js
@@ -7,3 +7,11 @@ console.log(output);
 console.log(output2);
 let output3=`The total pric is ${a+b+10} after including tax.`;
 console.log(output3);
+
+let c=prompt("Enter your age:");
+if(c>18){
+    console.log("You are adult.");
+}
+else{
+    console.log("You are minor.");
+}
---
+++ Javascript/traffic.js
@@ -0,0 +1,10 @@
+let color=prompt("Which Color??");
+if(color=='red'){
+    console.log("Stop!");
+}
+else if(color=='yellow'){
+    console.log("Be ready!");
+}
+else if(color=='green'){
+console.log("You can go.");
+}
