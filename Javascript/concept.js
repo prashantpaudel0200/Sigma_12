681945a0b4bba9bd23403f4facb8dd1b1638b301
Prashant Paudel <prashantpdl0200@gmail.com>
2026-06-30 18:02:08 +0545

    Template Literals unlocked

--- Javascript/concept.js
+++ Javascript/concept.js
@@ -1,1 +1,9 @@
 console.log("Hello World");
+let a=10;
+let b=20;
+let output="The total price is "+(a+b)+"ruppes."
+let output2="The difference is "+(b-a)+"ruppes"
+console.log(output);
+console.log(output2);
+let output3=`The total pric is ${a+b+10} after including tax.`;
+console.log(output3);
