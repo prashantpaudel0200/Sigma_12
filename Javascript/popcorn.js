48b7412308f30d31c05f1794d55eeaec33c21c2c
Prashant Paudel <prashantpdl0200@gmail.com>
2026-06-30 20:09:02 +0545

    PopCorn Problem solved

---
+++ Javascript/popcorn.js
@@ -0,0 +1,12 @@
+let size=prompt("Enter the size of PopCorn you want[ Xl / L / M / S ]:");
+if(size=='XL'){
+    console.log("The Price for your PopCorn is Rs 250.");
+} else if(size=='L'){
+    console.log("The Price for your PopCorn is Rs 200.");
+} else if(size=='M'){
+    console.log("The Price for your PopCorn is Rs 100.");
+} else if(size=='S'){
+    console.log("The Price for your PopCorn is Rs 50.");
+}
+console.log("Thankyou for ordering");
+console.log("See you soon!");
---
+++ Javascript/vote.js
@@ -0,0 +1,6 @@
+let age=19;
+if(age>=18){
+    console.log("You can vote");
+} else{
+    console.log("You cannot vote");
+}
