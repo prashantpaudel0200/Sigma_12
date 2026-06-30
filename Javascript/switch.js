df4bb320b0084096a92d6871d5be2315dd943b51
Prashant Paudel <prashantpdl0200@gmail.com>
2026-06-30 22:51:34 +0545

    Switch statements

---
+++ Javascript/switch.js
@@ -0,0 +1,11 @@
+let color = 'red';
+switch (color) {
+    case 'red':
+        console.log('color is red');
+        break;
+    case 'yellow':
+        console.log('Color is yellow');
+        break;
+    default:
+        console.log('Color is green');
+}
