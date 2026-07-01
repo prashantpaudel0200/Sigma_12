d18e64404321c9594e03a4b7363e15680e9c71fc
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-01 14:30:27 +0545

    method chaining

--- Javascript/practiceQs.js
+++ Javascript/practiceQs.js
@@ -1,6 +1,28 @@
-let num=100;
-if(num%10 ==0){
-    console.log("The number is divisible by 10");
-}else {
-    console.log("The number is not divisible by 10");
-}
+// let num=100;
+// if(num%10 ==0){
+//     console.log("The number is divisible by 10");
+// }else {
+//     console.log("The number is not divisible by 10");
+// }
+// let name=prompt("Enter your name: ");
+// let age=prompt("Enter your age: ");
+// alert(`${name} is ${age} years old`);
+
+// let q=prompt("Enter the quater number");
+// switch (q){
+//     case '1':
+//         console.log("January, February, March");
+//         break;
+//     case '2':
+//         console.log("April, May, June");
+//         break;
+//     case '3':
+//         console.log("Junly, Augest, September");
+//         break;
+//     case '4':
+//         console.log("Octuber, November, December");
+//         break;
+//     default:
+//         console.log("Refresh and enter valid Quater Number.");
+// }
+let password=prompt("Enter your password");
