a0cbfe16a8818e3ff66ad1f923bc909ddc55971a
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-04 21:00:51 +0545

    Forward and backward printing through loop

--- Javascript/loop.js
+++ Javascript/loop.js
@@ -6,3 +6,11 @@ console.log("Backward Count");
 for(i=15; i>=1; i--){
     console.log(i);
 }
+console.log("Even number between 2 to 10");
+for(i=2; i<=10; i+=2){
+    console.log(i);
+}
+console.log("Even number between 10 to 2");
+for(i=10; i>=2; i=i-2){
+    console.log(i);
+}
