28143bf60d0dc17f3d1e3eeaee39c4d9d46b9f1b
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-05 13:59:53 +0545

    Refactor the condition with break statement

--- Javascript/favMovie.js
+++ Javascript/favMovie.js
@@ -1,9 +1,12 @@
 let favMovie=prompt("Enter my favorite movie:");
 let guess=prompt("Enter your guess movie:");
-while((favMovie != guess) && (guess!="quit")){
+while((favMovie != guess)){
+    if(guess == "quit"){
+        console.log("You Quit!");
+        break;
+    }
     guess=prompt("Try again! Enter another:");
 }
 if(favMovie == guess){
     console.log("Congratulations you are correct.");
-} else
-    console.log("You Quit!");
+}
