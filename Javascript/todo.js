b3dee47bdfd1f2147766b08b60ea647d37e689c3
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-07 18:54:25 +0545

    ToDo app logic building

---
+++ Javascript/todo.js
@@ -0,0 +1,39 @@
+// let todo=[];
+// let req = prompt("Please enter your request:");
+
+// while(true) {
+//     if(req == "quit"){
+//         console.log("Quiting App");
+//         break;
+//     }
+//     if(req === "list"){
+//         console.log("--------------------");
+//         for(task of todo){
+//             console.log(tast);
+//         }
+//         console.log("--------------------");
+//     } else if(req === "add"){
+//         let task = prompt("Add new task:");
+//         todo.push(task);
+//         console.log("Task Added Successfully.");
+//     }
+//     //  else if( req === "delete");
+//     // req = prompt("Please enter your request:");
+// }
+let todo= [];
+let req = prompt("Please enter your request: ");
+while (true){
+    if(req == "quit"){
+        console.log("Quiting App");
+    }
+    if(req == "list"){
+        console.log("--------------------");
+        for(task of todo){
+             console.log(task);
+        }
+        console.log("--------------------");
+    }
+    else if(req == "add"){
+        let temp = prompt
+    }
+}
