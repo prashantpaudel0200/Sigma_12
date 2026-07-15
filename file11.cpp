9b5ef19b1497331ef7cb21cd132045877b1c9cde
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-15 13:34:47 +0545

    sum of two numbers

---
+++ file11.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int num1, num2;
+    cout<<"Enter Two number:";
+    cin>>num1>>num2;
+    int sum;
+    sum = num1 + num2;
+    cout<<"The sum is "<<sum;
+    return 0;
+}
