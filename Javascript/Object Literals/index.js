126c0e063e697e061631cccf84bec1d42a71bb5c
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-18 16:47:50 +0545

    Add/Update

--- Javascript/Object Literals/index.js
+++ Javascript/Object Literals/index.js
@@ -1,6 +1,17 @@
 let Student = {
     name : "Prashant Paudel",
     age : 19,
-    course : "B.Sc. CSIT"
+    course : "B.Sc. CSIT",
+    city : "Kathmandu"
 };
 console.log(Student);
+Student.gender = "Male";
+
+// const post = {
+//     username : '@prashant.pdl',
+//     content : 'My First Post',
+//     likes : 150,
+//     tags : ['@priyanshu', '@swodip123'],
+//     null : 354
+// };
+
