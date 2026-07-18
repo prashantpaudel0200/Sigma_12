1019d1f6a1ceedf1d8122969d19b6d612d33a7c8
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-18 17:02:12 +0545

    Nested Object

---
+++ Javascript/Object Literals/Nested Object/app.js
@@ -0,0 +1,15 @@
+const info = {
+     personal : {
+        name : 'Prashant Paudel',
+        age : 19,
+        city : 'Kathmandu',
+        gender : 'Male', 
+    },
+     academic : {
+        course : 'B.Sc. CSIT',
+        university : 'Tribhuvan University',
+        college : "St. Xavier's College",
+        semester : 2,
+    }
+};
+console.log(info);
