dade44aa0540975fdc028325bcbeb5c01cf00a4c
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-19 21:26:48 +0545

    New Project

---
+++ Javascript/Array of Objects/math.js
@@ -0,0 +1,11 @@
+let num = Math.random();
+num = num * 100;
+num = Math.floor(num);
+// console.log(num);
+num+=1;
+console.log(num);
+
+// Random Number between 45-50
+console.log("Random Number Between 45 to 50");
+let num2 = Math.floor(Math.random() * (50-45+1) ) + 45;
+console.log(num2);
