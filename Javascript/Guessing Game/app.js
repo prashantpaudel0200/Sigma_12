e0f5999eccd75bfcaa43fe746b11e59cd0a42c9a
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-21 20:34:44 +0545

    Final Commit

--- Javascript/Guessing Game/app.js
+++ Javascript/Guessing Game/app.js
@@ -1,18 +1,18 @@
 let max = prompt("Enter max limit");
-let num = Math.floor(Math.random() *max + 1);
+let num = Math.floor(Math.random() * max + 1);
 let guess = prompt("Enter your guess:")
-while(guess != num)
-    {
-        if(guess == 'quit'){
-            break;
-        }
-        if(guess != num){
-            guess = prompt("Wromg Guess!, Enter again");
-        }
+while (true) {
+    if (guess === 'quit') {
+        console.log("User Quit");
+        break;
     }
-    if(guess == num){
-            console.log(`Congratulations!!! You guessed it right. It was ${num}`);
-        }
-    if(guess == 'quit'){
-        console.log("Nice Try");
+
+    if (guess ==num) {
+        console.log(`Congratulations!!! You guessed it right. It was ${num}`);
+        break;
+    } else if (guess < num) {
+        guess = prompt("Wrong Guess!(You entered small), Enter again");
+    } else {
+        guess = prompt("Wrong Guess!(You entered bigger), Enter again");
     }
+}
