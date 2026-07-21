68288420d889b02b7be6e43873738ea85e6996b2
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-21 21:59:15 +0545

    Function in JS

---
+++ Javascript/Function/function.js
@@ -0,0 +1,17 @@
+function print1to5() {
+    for (let i = 0; i < 5; i++) {
+        console.log(i+1);
+    }
+}
+// print1to5();
+
+function isAdult(){
+    let age = 18;
+    if(age >= 18){
+        console.log("Person is adult");
+    }
+    else{
+        console.log("Person is not adult.");
+    }
+}
+isAdult();
