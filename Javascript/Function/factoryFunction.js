3fe9b32c1b118f0fbd9cdb4e7209d91e0901d878
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-23 22:22:43 +0545

    Factory Function

---
+++ Javascript/Function/factoryFunction.js
@@ -0,0 +1,13 @@
+function oddEvenTest(request){
+    if(request == "odd"){
+        return function(n){
+                console.log(!(n%2 == 0));
+        }
+    } else if(request == "even"){
+        return function(n){
+            console.log(n%2 ==0);
+        }
+    } else {
+        console.log("Wrong request");
+    }
+}
