b9951a33d6cdcee2ce668ac7e757b04373a621ab
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-23 22:34:13 +0545

    Array of object

---
+++ Javascript/Array of Objects/info.js
@@ -0,0 +1,16 @@
+const information = [
+    {
+        name : "Prashant Paudel",
+        class : "Bachelor's",
+        course : "B.Sc. CSIT",
+        college : "St. Xavier's College",
+        sem : 2
+    },
+    {
+        name : "Nishan Shrestha",
+        class : "Bachelor's",
+        course : "Civil Engineer",
+        college : "Undefined",
+        sem : "Yet to join"
+    }
+];
