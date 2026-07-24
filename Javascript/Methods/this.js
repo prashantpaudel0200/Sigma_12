dba6588525ba28dcc4777699ed79d9ed7e54048b
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-24 23:23:09 +0545

    Window object

--- Javascript/Methods/this.js
+++ Javascript/Methods/this.js
@@ -5,7 +5,13 @@ const student = {
     maths : 58,
     dl : 52,
     getAvg(){
+        console.log(this);
         let avg = (this.phy + this.maths + this.dl) /3;
-        return avg;
+        console.log(`${this.name} obtained average marks ${avg}.`);
     }
 };
+// function  getAvg(){
+//         console.log(this);
+//         let avg = (this.phy + this.maths + this.dl) /3;
+//         console.log(`${this.name} obtained average marks ${avg}.`);
+//     }
