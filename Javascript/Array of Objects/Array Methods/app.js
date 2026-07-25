981ab7d8ce3a058a9a87b18ce493984aca70afd8
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-25 15:16:20 +0545

    Array Methods

---
+++ Javascript/Array of Objects/Array Methods/app.js
@@ -0,0 +1,5 @@
+let arr = [11,12,23,3412,1231];
+let print = function(el){
+    console.log(el);
+}
+arr.forEach(print);
---
+++ Javascript/Array of Objects/Array Methods/object.js
@@ -0,0 +1,15 @@
+let arr = [{
+    name:"Prashant",
+    marks:97
+},
+{
+    name:"Jeeshant",
+    marks:90
+},
+{
+    name:"Manasvi",
+    marks:99
+}];
+arr.forEach((student) =>{
+    console.log(student.name);
+});
