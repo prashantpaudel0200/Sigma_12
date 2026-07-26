9edec2582067957b70973d6be55182c8c528f34b
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-26 15:51:46 +0545

    'filter' method in array

--- Javascript/Array of Objects/Array Methods/object.js
+++ Javascript/Array of Objects/Array Methods/object.js
@@ -14,10 +14,19 @@ let arr = [{
 //     console.log(student.name);
 // });
 
-let num = [10,22,33,44,55];
+let num = [10,22,33,44,55,2,19,87];
 let double = num.map((el) =>{
     return el*2;
 })
 let gpa = arr.map((el)=>{
     return el.marks/25;
+})
+
+
+let ans = num.filter((el) =>{
+    return !(el%2 ==0);
+});
+
+let less50 = num.filter((el)=>{
+    return el<50;
 })
