268185e83aba74c8ccecd984a12fa6be5d336233
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-27 01:22:22 +0545

    Use of 'spread'

--- Javascript/Array of Objects/Array Methods/practiceQs.js
+++ Javascript/Array of Objects/Array Methods/practiceQs.js
@@ -1,5 +1,7 @@
 let arr1=[3,45,56,34,-23,56];
 let arr2=[10,20,30,40,50];
+
+
 let ans = arr1.every((el)=> {
     return (el%10 ==0);
 }
@@ -18,3 +20,8 @@ function getMin(nums){
 });
 return min; 
 }
+
+let z= Math.min(...arr2);
+let y = Math.max(...arr1);
+let name = "Prashant Paudel"
+console.log(...name);
