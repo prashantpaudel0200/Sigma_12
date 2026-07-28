6903ab4b7497a08b51b49fd0a71c1497ba1dfd02
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-28 14:47:54 +0545

    Destructuring of Array and object

---
+++ Javascript/Array of Objects/Array Methods/destruct.js
@@ -0,0 +1,7 @@
+const student = {
+    name: "Prashant",
+    class : 12,
+    address : "Lalitpur",
+    subjects : ["Maths","Physics","Chemistry","Computer","English","Nepali"]
+};
+let {name: naam_van_naam, address:place= "Kathmandy"} = student;//output for place will be Lalitpur not ktm
---
+++ Javascript/Array of Objects/Array Methods/destructuring.js
@@ -0,0 +1,3 @@
+let names = ["Prashant", "Jeeshant","Durgesh", "Samir"];
+let [winner, runnerup, ...others] = names;
+console.log(winner, runnerup);//Prashant Jeeshant
