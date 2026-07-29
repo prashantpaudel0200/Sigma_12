192e12c20b061d71492ce3f6695857227e96271c
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-29 20:38:30 +0545

    setTimeout applied to dom to change src of images of class 'oldImg'

---
+++ Javascript/DOM/app.js
@@ -0,0 +1,13 @@
+let smallImages = document.getElementsByClassName("oldImg");
+    setTimeout(()=>{
+    smallImages[0].src = "assets/spiderman_img.png";
+    console.log(`The value of ${1} image is changed!`);
+    }, 4000);
+        setTimeout(()=>{
+    smallImages[1].src = "assets/spiderman_img.png";
+    console.log(`The value of ${2} image is changed!`);
+    }, 8000);
+        setTimeout(()=>{
+    smallImages[2].src = "assets/spiderman_img.png";
+    console.log(`The value of ${3} image is changed!`);
+    }, 10000);
