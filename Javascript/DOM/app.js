9eb651cf7b966e2afd42f133a064f09cd980bea5
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-30 22:07:35 +0545

    understanding innerText, innerHTML, and textContent

--- Javascript/DOM/app.js
+++ Javascript/DOM/app.js
@@ -1,13 +1,16 @@
-let smallImages = document.getElementsByClassName("oldImg");
-    setTimeout(()=>{
-    smallImages[0].src = "assets/spiderman_img.png";
-    console.log(`The value of ${1} image is changed!`);
-    }, 4000);
-        setTimeout(()=>{
-    smallImages[1].src = "assets/spiderman_img.png";
-    console.log(`The value of ${2} image is changed!`);
-    }, 8000);
-        setTimeout(()=>{
-    smallImages[2].src = "assets/spiderman_img.png";
-    console.log(`The value of ${3} image is changed!`);
-    }, 10000);
+// let smallImages = document.getElementsByClassName("oldImg");
+//     setTimeout(()=>{
+//     smallImages[0].src = "assets/spiderman_img.png";
+//     console.log(`The value of ${1} image is changed!`);
+//     }, 4000);
+//         setTimeout(()=>{
+//     smallImages[1].src = "assets/spiderman_img.png";
+//     console.log(`The value of ${2} image is changed!`);
+//     }, 8000);
+//         setTimeout(()=>{
+//     smallImages[2].src = "assets/spiderman_img.png";
+//     console.log(`The value of ${3} image is changed!`);
+//     }, 10000);
+
+let heading = document.querySelector('h1');
+// heading.innerHTML = (`<u>${heading.innerText}</u>`)
