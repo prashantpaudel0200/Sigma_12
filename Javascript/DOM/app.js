d847889dc4257fe7997d8206d2de3add9f8c8c3a
Prashant Paudel <prashantpdl0200@gmail.com>
2026-07-31 09:28:04 +0545

    changing style using DOM

--- Javascript/DOM/app.js
+++ Javascript/DOM/app.js
@@ -1,3 +1,38 @@
+let links = document.querySelectorAll(".box a");
+for(link of links){
+    link.style.color = "purple";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // let smallImages = document.getElementsByClassName("oldImg");
 //     setTimeout(()=>{
 //     smallImages[0].src = "assets/spiderman_img.png";
@@ -12,11 +47,11 @@
 //     console.log(`The value of ${3} image is changed!`);
 //     }, 10000);
 
-let heading = document.querySelector('h1');
-// heading.innerHTML = (`<u>${heading.innerText}</u>`)
+// let heading = document.querySelector('h1');
+// // heading.innerHTML = (`<u>${heading.innerText}</u>`)
 
-let image = document.querySelector('img')
-console.dir(image);
-console.dir(image.getAttribute('id'));
-// console.dir(image.setAttribute('id', 'spiderimage'));
-image.setAttribute('src', 'assets/creation_1.png');
+// let image = document.querySelector('img')
+// console.dir(image);
+// console.dir(image.getAttribute('id'));
+// // console.dir(image.setAttribute('id', 'spiderimage'));
+// image.setAttribute('src', 'assets/creation_1.png');
