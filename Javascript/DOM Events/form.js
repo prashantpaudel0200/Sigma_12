1d8cabed7a06a2c06ab5f756f4eb02cfbf5b4956
Prashant Paudel <prashantpdl0200@gmail.com>
2026-08-01 23:56:50 +0545

    accessing value in form

--- Javascript/DOM Events/form.js
+++ Javascript/DOM Events/form.js
@@ -2,4 +2,10 @@ let submit = document.querySelector("form button");
 submit.addEventListener("click", function(event){
     event.preventDefault();
     alert("You are submmiting this form");
+
+
+
+    let inp = document.querySelector("input");
+    console.dir(inp);
+    console.log(inp.value);
 })
