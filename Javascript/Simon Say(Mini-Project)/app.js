cfb6257d81671f480674dd56477b1d1c049c1ec1
Prashant Paudel <prashantpdl0200@gmail.com>
2026-08-02 21:50:54 +0545

    gameSeq is updated and major changes are done in simon say game

--- Javascript/Simon Say(Mini-Project)/app.js
+++ Javascript/Simon Say(Mini-Project)/app.js
@@ -1,3 +1,4 @@
+
 let gameSeq = [];
 let userSeq = [];
 let btns = ["red", "yellow", "green", "purple"];
@@ -18,15 +19,44 @@ function btnFlash(btn){
     btn.classList.add("flash");
     setTimeout(function(){
         btn.classList.remove("flash");
-    }, 250);
+    }, 150);
 }
 
 function levelUp(){
     level++;
+    userSeq = [];
     h2.innerText =`Level ${level}`;
     //choose random button
     let randIdx = Math.floor(Math.random()* 4);
     let randColor = btns[randIdx];
-    let randbtn = document.querySelector(`.${randColor}`)
+    let randbtn = document.querySelector(`.${randColor}`);
+    gameSeq.push(randColor);
+    console.log(gameSeq);
     btnFlash(randbtn);
+}
+function checkAns(idx){
+    // console.log("Current Level: ",level);
+    if(gameSeq[idx] === userSeq[idx]){
+        if(gameSeq.length == userSeq.length){
+            setTimeout(levelUp, 1000);
+        }
+
+    } else{
+        h2.innerText = 'Game Over! Press any key to start.';
+    }
+}
+
+function btnPress(){
+    console.log(this);
+    let btn = this;
+    btnFlash(btn);
+    let userColor = btn.getAttribute("id");
+    console.log(userColor);
+    userSeq.push(userColor);
+    checkAns(userSeq.length-1);
+}
+
+let allbtns = document.querySelectorAll(".btn");
+for(btn of allbtns){
+    btn.addEventListener("click", btnPress);
 }
