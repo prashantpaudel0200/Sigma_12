674700808ad7e00671b8faf3862d6c6d607429d2
Prashant Paudel <prashantpdl0200@gmail.com>
2026-08-03 18:13:22 +0545

    JS is single Threaded

---
+++ Javascript/Call Stack/singleThreaded.js
@@ -0,0 +1,10 @@
+setTimeout(() => {
+    console.log("1.Completed");
+},2000);
+setTimeout(()=>{
+    console.log("2.Completed");
+},2000);
+console.log("Hi I am Prashant");
+//This Program shows the asynchronous nature of JS 
+//JavaScript is a synchronous, single-threaded programming language by default
+// Synchronous = Executing one by one(line by line)
