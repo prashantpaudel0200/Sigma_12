0b27dc53dd159f938d0fff69577a9e2b3dd52be3
Prashant Paudel <prashantpdl0200@gmail.com>
2026-08-03 19:17:39 +0545

    Promises

---
+++ Javascript/Call Stack/promises.js
@@ -0,0 +1,24 @@
+function save2Db(data, success, failure){
+    let internetSpeed = Math.floor(Math.random()*10)+1;
+    if(internetSpeed>4){
+        success();
+
+    } else{
+        failure();
+        
+    }
+};
+
+save2Db("Prashant Paudel is my name",
+        ()=> {
+        console.log("SUCESS1: DATA IS SAVED");
+        save2Db("Hello World", ()=>{
+            console.log("SUCESS2: DATA IS SAVED");
+        }, ()=>{
+            console.log("FAILURE2: DATA IS NOT SAVED.")
+        });
+        }, 
+        () => {
+        console.log("FAILURE: DATA IS NOT SAVED. Weak Connection");
+        }
+);
