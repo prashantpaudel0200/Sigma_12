c72ef54c36d3ab8c72c1cdab1f1172d391b85e6f
Prashant Paudel <prashantpdl0200@gmail.com>
2026-08-04 23:19:01 +0545

    Displaying a object name 'student' in JSON format by stringify method

--- Javascript/API/app.js
+++ Javascript/API/app.js
@@ -2,8 +2,14 @@ let jsonRes =
        '{"fact":"Long, muscular hind legs enable snow leopards to leap seven times their own body length in a single bound.","length":106}'
 
 let validRes = JSON.parse(jsonRes);
-console.log(validRes);
-console.log("validRes is a object now, and we can access key-value pair.");
-console.log(validRes.fact);
+// console.log(validRes);
+// console.log("validRes is a object now, and we can access key-value pair.");
+// console.log(validRes.fact);
 console.log(validRes.length);
+
+let student = {
+    name : "Prashant Paudel",
+    course : "BSC CSIT"
+};
+console.log(JSON.stringify(student));
 
