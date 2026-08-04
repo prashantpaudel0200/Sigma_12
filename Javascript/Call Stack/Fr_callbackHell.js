e66d9743517ae95cc89583f418a3ae54cc5a56f6
Prashant Paudel <prashantpdl0200@gmail.com>
2026-08-04 19:26:22 +0545

    Result & errors in promises

--- Javascript/Call Stack/Fr_callbackHell.js
+++ Javascript/Call Stack/Fr_callbackHell.js
@@ -18,30 +18,70 @@
 //     }
 // );
 
-function save2Db(data){
+// function save2Db(data){
+//     return new Promise((resolve, reject)=>{
+//         let internetSpeed = Math.floor(Math.random()*10)+1;
+//         if(internetSpeed>4){
+//                resolve("Data is saved");
+//         } else{
+//                reject("Failure: weak connection");  
+//     }
+//     });
+// };
+
+
+// save2Db("Prashant Paudel")
+//  .then((result)=>{
+//     console.log("Data1 saved");
+//     console.log(result)
+//     return save2Db("Pravin Paudel");
+//  })
+//   .then((result)=>{
+//     console.log("Data2 Saved");
+//     console.log(result)
+//     return save2Db("Laxmi Paudel");
+//   })
+//   .then((result)=>{
+//     console.log("Data3 Saved");
+//     console.log(result)
+//   })
+//   .catch((error)=>{
+//     console.log("Promise was rejected");
+//     console.log(error);
+//   });
+
+
+
+
+
+function savetoData(data){
     return new Promise((resolve, reject)=>{
-        let internetSpeed = Math.floor(Math.random()*10)+1;
-        if(internetSpeed>4){
-               resolve("Data is saved");
+        let num = Math.floor(Math.random()* 10)+1;
+        if(num>4){
+            resolve("Success: Data is saved");
         } else{
-               reject("Data is not saved, weak connection");  
-    }
+            reject("Failure: Data is not saved");
+        }
     });
 };
 
 
-let result = save2Db("Prashant Paudel")
- .then(()=>{
-    console.log("Data 1 saved");
-    return save2Db("Pravin Paudel");
- })
-  .then(()=>{
-    console.log("Data2 Saved");
-    return save2Db("Laxmi Paudel");
+savetoData("My name is Prashant Paudel")
+  .then((result)=>{
+    console.log(result);
+    console.log("Data1: is saved");
+    return savetoData("I am saving HELLO WORLD");
   })
-  .then(()=>{
-    console.log("Data 3 is saved");
+  .then((result) =>{
+    console.log(result);
+    console.log("Data2: is saved");
+    return savetoData("I am saving 3rd data");
   })
-  .catch(()=>{
-    console.log("Failure, data is not saved!");
-  });
+  .then((result)=>{
+    console.log(result);
+    console.log("DATA3: is saved");
+  })
+  .catch((error)=>{
+    console.log("DATA IS NOT SAVED")
+    console.log(error);
+  })
