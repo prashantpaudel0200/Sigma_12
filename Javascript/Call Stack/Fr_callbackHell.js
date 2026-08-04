142570be9fa7ff9c27168a92302035b8bd1aea60
Prashant Paudel <prashantpdl0200@gmail.com>
2026-08-04 18:12:04 +0545

    First use of Promise, new Promise(resolve, reject)

--- Javascript/Call Stack/Fr_callbackHell.js
+++ Javascript/Call Stack/Fr_callbackHell.js
@@ -1,29 +1,32 @@
-function save2Db(data, success, failure){
-    let internetSpeed = Math.floor(Math.random()*10)+1;
-    if(internetSpeed>4){
-        success();
 
-    } else{
-        failure();
-        
+// save2Db("Prashant Paudel is my name",
+//     () => {
+//         console.log("SUCESS1: DATA IS SAVED");
+//         save2Db("Hello World", () => {
+//             console.log("SUCESS2: DATA IS SAVED");
+//             save2Db("Pravin Paudel", () => {
+//                 console.log("SUCESS3: DATA IS SAVED");
+//             }, () => {
+//                 console.log("FAILURE3: DATA IS NOT SAVED");
+//             })
+//         }, () => {
+//             console.log("FAILURE2: DATA IS NOT SAVED.")
+//         });
+//     },
+//     () => {
+//         console.log("FAILURE: DATA IS NOT SAVED. Weak Connection");
+//     }
+// );
+
+
+
+function save2Db(data){
+    return new Promise((resolve, reject)=>{
+        let internetSpeed = Math.floor(Math.random()*10)+1;
+        if(internetSpeed>4){
+               resolve("Data is saved");
+        } else{
+               reject("Data is not saved, weak connection");  
     }
+    });
 };
-
-save2Db("Prashant Paudel is my name",
-    () => {
-        console.log("SUCESS1: DATA IS SAVED");
-        save2Db("Hello World", () => {
-            console.log("SUCESS2: DATA IS SAVED");
-            save2Db("Pravin Paudel", () => {
-                console.log("SUCESS3: DATA IS SAVED");
-            }, () => {
-                console.log("FAILURE3: DATA IS NOT SAVED");
-            })
-        }, () => {
-            console.log("FAILURE2: DATA IS NOT SAVED.")
-        });
-    },
-    () => {
-        console.log("FAILURE: DATA IS NOT SAVED. Weak Connection");
-    }
-);
