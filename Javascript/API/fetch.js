186f3401cc1f40749847eb7c3aa35a957c717d9f
Prashant Paudel <prashantpdl0200@gmail.com>
2026-08-05 19:13:22 +0545

    using fetch with async-await

--- Javascript/API/fetch.js
+++ Javascript/API/fetch.js
@@ -1,20 +1,34 @@
 let url = "https://catfact.ninja/fact";
 
-fetch(url)
- .then((response)=>{
-    return response.json();
-    })
-  .then((data)=>{
-    console.log("Data1 - ",data.fact);
-    return fetch(url);
-  })  
-  .then((response)=>{
-    return response.json();
-  })
-  .then((data2)=>{
-    console.log("Data2- ",data2.fact);
-  })
- .catch((error) =>{
-    console.log("Error- ",error);
- })
+// fetch(url)
+//  .then((response)=>{
+//     return response.json();
+//     })
+//   .then((data)=>{
+//     console.log("Data1 - ",data.fact);
+//     return fetch(url);
+//   })  
+//   .then((response)=>{
+//     return response.json();
+//   })
+//   .then((data2)=>{
+//     console.log("Data2- ",data2.fact);
+//   })
+//  .catch((error) =>{
+//     console.log("Error- ",error);
+//  })
+async function getFacts(){
+    let res = await fetch(url);
+    console.log(res);
+    let fine =await res.json();
+    console.log(fine.fact);
+}
+
+
+
+
+
+
+
+
  console.log("I am Happy.")
