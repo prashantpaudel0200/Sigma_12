53e7a15ddfec335698af400e0c19470170de176a
Prashant Paudel <prashantpdl0200@gmail.com>
2026-08-05 18:53:59 +0545

    My First API request

--- Javascript/API/fetch.js
+++ Javascript/API/fetch.js
@@ -2,8 +2,19 @@ let url = "https://catfact.ninja/fact";
 
 fetch(url)
  .then((response)=>{
-    console.log(response)
- })
+    return response.json();
+    })
+  .then((data)=>{
+    console.log("Data1 - ",data.fact);
+    return fetch(url);
+  })  
+  .then((response)=>{
+    return response.json();
+  })
+  .then((data2)=>{
+    console.log("Data2- ",data2.fact);
+  })
  .catch((error) =>{
-    console.log("Error Found!");
+    console.log("Error- ",error);
  })
+ console.log("I am Happy.")
