faa83bf69b8f98d5a70ba2f6467d7ab2d459fc3b
Prashant Paudel <prashantpdl0200@gmail.com>
2026-08-06 19:35:10 +0545

    sending headers with API requests

---
+++ Javascript/API/axiosHeaders.js
@@ -0,0 +1,6 @@
+let url = "https://icanhazdadjoke.com/";
+const config = { headers:{Accept: "application/json"} };
+async function getJoke(){
+    let req = await axios.get(url, config);
+    console.log(req.data.joke);
+}
