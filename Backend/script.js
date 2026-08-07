62f2f2f5e2ef63ac7bb38d8ba83ea8ced998746d
Prashant Paudel <prashantpdl0200@gmail.com>
2026-08-07 22:04:10 +0545

    use of 'process.argv'

--- Backend/script.js
+++ Backend/script.js
@@ -1,5 +1,10 @@
-let n=5;
+// let n=5;
 
-for(let i = 0; i<5; i++){
-    console.log("Hello ", i+1);
+// for(let i = 0; i<5; i++){
+//     console.log("Hello ", i+1);
+// }
+
+let arg = process.argv;
+for(let i = 2; i< arg.length; i++){
+    console.log("Hello to ", arg[i]);
 }
