65d9fb391a343f2c3d90cbeab0a5e1124150d43b
Prashant Paudel <prashantpdl0200@gmail.com>
2026-08-09 13:15:53 +0545

    Routing

--- Backend/ExpressDir/index.js
+++ Backend/ExpressDir/index.js
@@ -7,13 +7,24 @@ app.listen(port, ()=>{
     console.log(`App is listining from port ${port}`);
 });
 
-app.use((req, res)=>{
+app.get('/home',(req, res)=>{
     // console.log(req);
     console.log("request received");
-    // res.send({
-    //     name:"Apple",
-    //     color:"Red"
-    // });
+    res.send({
+        name:"Apple",
+        color:"Red"
+    });
+});
+app.get('/fruits',(req, res)=>{
+    console.log("request received");
     let code = "<h1>Fruits</h1><ul><li>Apple</li><li>Orange</li></ul>"
     res.send(code);
 });
+
+app.get("*splat", (req, res)=>{
+    res.send("Error-404 Page not Found");
+});
+
+app.post("/",(req, res)=>{
+    res.send("You send a post request.");
+})
