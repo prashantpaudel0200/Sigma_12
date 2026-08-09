7c80dc5a27e8e245f05f058c67e8d8ada24c7269
Prashant Paudel <prashantpdl0200@gmail.com>
2026-08-09 12:49:19 +0545

    Sending my first response

--- Backend/ExpressDir/index.js
+++ Backend/ExpressDir/index.js
@@ -8,5 +8,12 @@ app.listen(port, ()=>{
 });
 
 app.use((req, res)=>{
+    // console.log(req);
     console.log("request received");
+    // res.send({
+    //     name:"Apple",
+    //     color:"Red"
+    // });
+    let code = "<h1>Fruits</h1><ul><li>Apple</li><li>Orange</li></ul>"
+    res.send(code);
 });
