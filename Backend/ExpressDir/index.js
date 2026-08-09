db5c958c5de587bd73975ae6c5fe82d95b76acee
Prashant Paudel <prashantpdl0200@gmail.com>
2026-08-09 14:10:04 +0545

    Use of Query String

--- Backend/ExpressDir/index.js
+++ Backend/ExpressDir/index.js
@@ -15,16 +15,33 @@ app.get('/home',(req, res)=>{
         color:"Red"
     });
 });
-app.get('/fruits',(req, res)=>{
-    console.log("request received");
-    let code = "<h1>Fruits</h1><ul><li>Apple</li><li>Orange</li></ul>"
-    res.send(code);
-});
-
-app.get("*splat", (req, res)=>{
-    res.send("Error-404 Page not Found");
-});
-
-app.post("/",(req, res)=>{
-    res.send("You send a post request.");
+app.get('/:username/:id',(req, res)=>{
+    let {username, id} = req.params;
+    res.send(`<h1>Welcome to the page of @${username}</h1>`);
+    console.log(req.params);
 })
+
+
+app.get("/search",(req, res)=>{
+    let {q} = req.query;
+    if(!q){
+        res.send("Nothing search");
+    }
+    res.send(`<h1>Seach Result for: ${q}</h1>`);
+})
+
+
+
+// app.get('/fruits',(req, res)=>{
+//     console.log("request received");
+//     let code = "<h1>Fruits</h1><ul><li>Apple</li><li>Orange</li></ul>"
+//     res.send(code);
+// });
+
+// app.get("*splat", (req, res)=>{
+//     res.send("Error-404 Page not Found");
+// });
+
+// app.post("/",(req, res)=>{
+//     res.send("You send a post request.");
+// })
