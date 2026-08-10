a6cb4406062e508424eb906b23d02dcf31b0e56a
Prashant Paudel <prashantpdl0200@gmail.com>
2026-08-10 16:41:59 +0545

    Passing Data to EJS (Interpolation)

---
+++ Backend/EJSdir/Interpolation/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 8080;
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+
+app.get("/", (req, res)=>{
+    res.render("home.ejs");
+})
+app.get("/rolldice", (req, res)=>{
+    let diceValue = Math.floor(Math.random()*6)+1;
+    res.render("rolldice.ejs", {num: diceValue});
+});
+
+
+app.listen(port, ()=>{
+    console.log("App is listening");
+})
