1243d288f25d35f3a010b2dc6151520301bd4182
Prashant Paudel <prashantpdl0200@gmail.com>
2026-09-19 21:15:22 +0545

    routing through ejs

--- Backend/EJSdir/Restart/index.js
+++ Backend/EJSdir/Restart/index.js
@@ -9,6 +9,17 @@ app.set("views", path.join(__dirname, "/views"));
 app.get("/", (req, res)=>{
     res.render("home.ejs");
 });
+
+app.get("/rollDice", (req, res)=>{
+    let diceNum = Math.floor(Math.random()*6+1);
+    res.render("rolldice", {diceNum});
+});
+
+app.get("/ig/:instagram", (req, res)=>{
+    let { username } = req.params;
+    console.log(username);
+})
+
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`);
 });
