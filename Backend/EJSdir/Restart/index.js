5e7aed01577e61c47dceaf34574fc79c4924ca75
Prashant Paudel <prashantpdl0200@gmail.com>
2026-09-20 14:45:52 +0545

    use of loops in ejs

--- Backend/EJSdir/Restart/index.js
+++ Backend/EJSdir/Restart/index.js
@@ -16,8 +16,9 @@ app.get("/rollDice", (req, res)=>{
 });
 
 app.get("/ig/:username", (req, res)=>{
+    const followers = ["adam", "eve", "michiale", "bob"];
     let { username } = req.params;
-    res.render("instagram.ejs", {username})
+    res.render("instagram.ejs", {username, followers})
 })
 
 app.listen(port, ()=>{
